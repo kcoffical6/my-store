fc98a07e338d165b53d8951adea35af8bbdc4f90
kcoffical6 <99400211+kcoffical6@users.noreply.github.com>
2023-09-13 11:26:29 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f╭─《 🔰𝔸𝔹𝕆𝕌𝕋 𝕄𝔼🔰 》\n├  CREATOR: <a href='tg://settings'>Hacker💻</a>\n├  CHANNEL: <a href='https://t.me/+kQS5TtxyNBVjNjg1'>Sb Kan-Serial</a>, <a href='https://t.me/+kQS5TtxyNBVjNjg1'>𝙺𝙰𝙽𝙽𝙰𝙳𝙰 𝙲𝙷𝙰𝙽𝙽𝙴𝙻𝚂 𝙻𝙸𝙽𝙺</a>\n├ LANGUAGE USED: Python\n╰  𝐓𝐇𝐀𝐍𝐊 𝐘𝐎𝐔❤️</b>",
+            text = f"╭─《 🔰𝔸𝔹𝕆𝕌𝕋 𝕄𝔼🔰 》\n├  CREATOR: <a href='tg://settings'>Hacker💻</a>\n├  CHANNEL: <a href='https://t.me/+kQS5TtxyNBVjNjg1'>Sb Kan-Serial</a>, <a href='https://t.me/+kQS5TtxyNBVjNjg1'>𝙺𝙰𝙽𝙽𝙰𝙳𝙰 𝙲𝙷𝙰𝙽𝙽𝙴𝙻𝚂 𝙻𝙸𝙽𝙺</a>\n├ LANGUAGE USED: Python\n╰  𝐓𝐇𝐀𝐍𝐊 𝐘𝐎𝐔❤️</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
