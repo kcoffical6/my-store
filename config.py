3a62b47c936b2212975580917e6fa5b7b7d6f38b
kcoffical6 <99400211+kcoffical6@users.noreply.github.com>
2023-09-15 16:04:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "╔┓┏╦━━╦┓╔┓╔━━-\n║┗┛║┗━╣┃║┃║ ^ ^ ✦\n║┏┓║┏━╣┗╣┗╣╰╯║\n╚┛┗╩━━╩━╩━╩━━╝\n\n\n<b>Hello {first}\nI am a bot which will provide you files instantly which are shared in @Kan_Serial and @SB_SERIALS </b>\n\n⚠Kindly Please join my channel - Serial Adda and Kannada Channels Link / ದಯವಿಟ್ಟು ನನ್ನನ್ನು ಬಳಸಲು ಚಾನಲ್‌ಗೆ ಸೇರಿಕೊಳ್ಳಿ⚠")
+START_MSG = os.environ.get("START_MESSAGE", "╔┓┏╦━━╦┓╔┓╔━━-\n║┗┛║┗━╣┃║┃║ ^ ^ ✦\n║┏┓║┏━╣┗╣┗╣╰╯║\n╚┛┗╩━━╩━╩━╩━━╝\n\n\n<b>Hello {first}\nI am a bot which will provide you files instantly which are shared in @Kan_Serial and @SB_SERIALS </b>\n\n⚠Kindly Please join my channel / ದಯವಿಟ್ಟು ನನ್ನನ್ನ ಚಾನಲ್‌ಗೆ ಸೇರಿಕೊಳ್ಳಿ⚠")
 try:
     ADMINS=[]
     for x in (os.environ.get("ADMINS", "").split()):
